c67aad759d0d81cbfb57d3cd8f976431ab15a52f
Alfredo Colantonio <alfredo.colantonio95@gmail.com>
2021-07-24 11:53:39 +0200

    Added privacy message and test logo

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,9 +27,10 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
           <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css" />
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
         </Helmet>
         {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
-        <div className="container">
+        <div className="container-fluid" style={{'padding-left':0, 'padding-right':0}}>
           {children}
         </div>
       </>
